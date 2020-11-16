767ac2d2b4dc6fd8dea968e6731ea079bee69876
Volodymyr77 <gagarinvov@gmail.com>
2020-11-16 23:55:47 +0200

    data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import React, {Component, useState, useEffect} from "react";
+
+// class App extends Component {
+//   state = {
+//     count: 0,
+//     a: 1,
+//     b: 2,
+//   }
+//
+//   onInc = () => {
+//     // this.setState({});
+//    this.setState((prevState) => ({
+//      count: this.state.count + 1
+//    }))
+//   }
+
+  // render() {
+  //   const {count} = this.state;
+  //   return <div className="App">
+  //     <h1> {count}</h1>
+  //     <button onClick={this.onInc}>inc</button>
+  //
+  //   </div>
+  // }
+// }
+
+ function App() {
+     const [count, setCountVal] = useState(1);
+     const [todo, setTodo] = useState(null);
+
+     const fetchTodo = async () => {
+         const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${count}`);
+         const data = await response.json();
+
+
+         setTodo(data);
+         console.log(data);
+     }
+
+
+     useEffect(() => {
+         fetchTodo(count);
+
+     }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1> {count}</h1>
+        {!!todo && (
+        <div>
+            <h2>{todo.title}</h2>
+            <h2>{todo.completed ? "task is done" : "task is not done yet"}</h2>
+        </div>
+        )
+        }
+
+     <button
+         onClick={() => {
+             setCountVal((prevCount) => prevCount + 1);
+         }}
+     >
+         inc
+     </button>
+   </div>
   );
 }
 
